c828a972d71a4d7f814f7bf933001054b5a790cf
tatwd <leoking9641@hotmail.com>
2018-02-04 15:29:30 +0800

    Reset default value and add some comments

--- js/ajax.js
+++ js/ajax.js
@@ -4,10 +4,10 @@
  * @param {String} url 请求路径
  * @param {Object} settings 参数配置 
  */
-const ajax = function (url, settings) {
+const ajax = function (url, settings = {}) {
 
     // set default values
-    ({ method = 'GET', responseType = '', header = {}, timeout = 0, data = null, async = true } = settings || {});
+    ({ method = 'GET', responseType = '', header = {}, timeout = 0, data = null, isAsync = true } = settings);
 
     // get XMLHttpRequest object
     let getXhr = () => new XMLHttpRequest() || new ActiveXObject("Microsoft.XMLHTTP");
@@ -17,26 +17,27 @@ const ajax = function (url, settings) {
             this._data = data;
         }
         
+        getText() {
+            return this._data;
+        }
+
         getJson() {
             let __data = typeof this._data === 'object'
             ? this._data
             : JSON.parse(this._data);
-            
             
             return __data === null
                 ? __data
                 : (__data.d && __data.d !== '' ? JSON.parse(__data.d) : __data); 
         }
 
-        getText() {
-            return this._data;
-        }
+        // other format methods here
     }
 
     return new Promise((resolve, reject) => {
         const xhr = getXhr();
         
-        xhr.open(method, url, async);
+        xhr.open(method, url, isAsync);
 
         xhr.onreadystatechange = function () {
             if (this.readyState !== 4) return;
